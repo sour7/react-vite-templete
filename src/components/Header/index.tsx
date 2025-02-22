698c6f515c997cd3bdfd3b805699f3b097f27dbc
sour7 <sourabhdev273@gmail.com>
2025-02-22 22:08:32 +0530

    add pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,57 @@
-import {JSX} from 'react'
+import { AppShell, ActionIcon, Group, NavLink as MantineNavLink, Title } from "@mantine/core";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
+import { NavLink, useLocation } from "react-router-dom";
+import type { MantineColorScheme } from "@mantine/core";
+import React from "react";
 
-export default function Header(): JSX.Element {
+export interface HeaderProps {
+  colorScheme: MantineColorScheme;
+  onToggleColorScheme: () => void;
+}
+
+const NavLinks: { path: string; label: string }[] = [
+  {
+    path: "/",
+    label: "Home",
+  },
+  {
+    path: "/page-a",
+    label: "PageA",
+  },
+  {
+    path: "/page-b",
+    label: "PageB",
+  },
+  {
+    path: "/broken",
+    label: "Broken",
+  },
+];
+
+export function Header({ colorScheme, onToggleColorScheme }: HeaderProps): React.JSX.Element {
+  const location = useLocation();
+
   return (
-    <header>
-      <h1>My React App</h1>
-    </header>
-  )
+    <AppShell.Header p="xs">
+      <Group justify="space-between" align="center">
+        <Title order={5}>Electron</Title>
+        <Group wrap="nowrap">
+          {NavLinks.map(({ path, label }) => {
+            return (
+              <MantineNavLink
+                key={path}
+                component={NavLink}
+                active={location.pathname === path}
+                to={path}
+                label={label}
+              />
+            );
+          })}
+        </Group>
+        <ActionIcon onClick={onToggleColorScheme} variant="default">
+          {colorScheme === "dark" ? <IconSun /> : <IconMoonStars />}
+        </ActionIcon>
+      </Group>
+    </AppShell.Header>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Router } from './routes.tsx'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <MantineProvider>
        <RouterProvider router={Router} />
+       </MantineProvider>
   </StrictMode>,
 )
