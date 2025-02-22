6b0a952163b8e5e9fbd607db3739967110ebc3d8
sour7 <sourabhdev273@gmail.com>
2025-02-22 22:55:22 +0530

    setup sb

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
-import { Router } from "./routes.tsx";
-import { MantineProvider } from "@mantine/core";
+import { Router } from "./routes";
+import { MantineProvider, createTheme } from "@mantine/core";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import "@fontsource/raleway/400.css";
+import "@fontsource/raleway/500.css";
+import "@fontsource/raleway/600.css";
+import "@fontsource/raleway/700.css";
+import "@fontsource/raleway/800.css";
+import "@fontsource/raleway/900.css";
 
-createRoot(document.getElementById("root")!).render(
+const MantineTheme = createTheme({
+  fontFamily: "Raleway, sans-serif", // Updated font-family to Raleway
+  primaryColor: "red",
+}); // Ref: https://mantine.dev/theming/theme-object/#store-theme-override-object-in-a-variable
+
+const root = createRoot(document.getElementById("root") as HTMLDivElement);
+root.render(
   <StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={MantineTheme}>
       <RouterProvider router={Router} />
     </MantineProvider>
   </StrictMode>,
